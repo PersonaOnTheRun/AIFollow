10d0954a01047e81f16320c4c77230cee3b788d0
PersonaOnTheRun <cammilligan@Camerons-MacBook-Pro.local>
2017-02-05 19:26:02 -0500

    update

---
+++ AIFollow.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  5 14:52:47 2017
+
+@author: cammilligan
+"""
+
+from TwitterFollowBot import TwitterBot
+
+my_bot = TwitterBot('/Users/cammilligan/Dropbox/TwitterMadness/config.txt')
+my_bot.sync_follows()
+my_bot.auto_follow("#machinelearning")
